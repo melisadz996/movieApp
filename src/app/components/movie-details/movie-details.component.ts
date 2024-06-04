c715ea1385456681de6ff1d2304757262f5910db
melisadz996 <melisadzeko@outlook.com>
2024-06-04 12:49:56 +0200

    Implemented loading indicators and provided clear error messages for API request.

--- src/app/components/movie-details/movie-details.component.ts
+++ src/app/components/movie-details/movie-details.component.ts
@@ -3,6 +3,9 @@ import { ActivatedRoute } from '@angular/router';
 import { MovieService } from '../../services/movie.service';
 import { MovieDetails } from '../../models/movie.model';
 import { Router } from '@angular/router';
+import { throwError } from 'rxjs';
+import { catchError, finalize } from 'rxjs/operators';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-movie-details',
@@ -11,6 +14,8 @@ import { Router } from '@angular/router';
 })
 export class MovieDetailsComponent implements OnInit {
   movieDetails: MovieDetails | undefined;
+  loading: boolean = false;
+  errorMessage: string = '';
 
   constructor(
     private route: ActivatedRoute,
@@ -20,12 +25,22 @@ export class MovieDetailsComponent implements OnInit {
 
   ngOnInit(): void {
     const id = +this.route.snapshot.paramMap.get('id')!;
-    this.movieService.getMovieDetails(id).subscribe((details: MovieDetails) => {
-      this.movieDetails = details;
-    });
+    this.loading = true;
+    this.movieService.getMovieDetailsWithLoading(id).subscribe(
+      (details: MovieDetails) => {
+        this.movieDetails = details;
+      },
+      (error: HttpErrorResponse) => {
+        this.errorMessage = 'Error fetching movie details. Please try again later.';
+        this.loading = false;
+      },
+      () => {
+        this.loading = false;
+      }
+    );
   }
 
   goBackToList(): void {
     this.router.navigate(['']);
-  }
+  } 
 }
